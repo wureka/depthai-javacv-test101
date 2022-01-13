353821dfda5936f7433afacaf2bec3b70c4953fb
alexji <wureka@gmail.com>
2022-01-13 17:27:07 +0800

    init

--- src/main/java/org/example/Demo.java
+++ src/main/java/org/example/Demo.java
@@ -124,6 +124,7 @@ public class Demo {
 
 
     public static void main(String[] args) throws Exception {
+        System.setProperty("org.bytedeco.javacpp.logger.debug", "true");
         // demoJavaCv();
         demoDepthaiRgbPreview();
     }
